19428568fa7e005e457123f34c767537e85671e1
Anthony <anthony-bonta@gfe.energy>
2024-06-27 13:53:22 -0500

    chore: default router

--- packages/user-web/src/index.tsx
+++ packages/user-web/src/index.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { UserApp } from './app/user-app';
 
 const container = createRoot(document.getElementById('user-app')!);
 
 container.render(
   <StrictMode>
-    <UserApp />
+    <BrowserRouter>
+      <UserApp />
+    </BrowserRouter>
   </StrictMode>,
 );
