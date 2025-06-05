3c8787d250c44bca1e59aaa35937c99825ef0ddc
Cesar Victorio <cesarvictorio@Cesars-MacBook-Pro.local>
2025-06-05 11:36:19 -0700

    update testing structure, validate form submission

--- packages/user-web/src/user/user-page.test.tsx
+++ packages/user-web/src/user/user-page.test.tsx
@@ -46,6 +46,9 @@ describe('User Page', () => {
     const getInput = (name: string) =>
       Promise.resolve(screen.getByLabelText(new RegExp(name, 'i')));
 
+    const getSelect = (name: string) =>
+      Promise.resolve(screen.getByRole('combobox', { name: new RegExp(name, 'i') }));
+
     const getHeader = () => Promise.resolve(screen.getByRole('heading', { name: /user profile/i }));
 
     const getCurrentPath = () => currentLocation?.pathname;
@@ -53,10 +56,17 @@ describe('User Page', () => {
     const getCreateUserButton = () =>
       Promise.resolve(screen.getByText<HTMLButtonElement>('Create User'));
 
-    const populateField = async (fieldName: string, value: string) => {
+    const getUserTypeSelect = async () => await getSelect('user type');
+
+    const populateInput = async (fieldName: string, value: string) => {
       const field = await getInput(fieldName);
       await userEvent.clear(field);
       await userEvent.type(field, value);
+    };
+
+    const populateSelect = async (fieldName: string, value: string) => {
+      const selectElement = await getSelect(fieldName);
+      await userEvent.selectOptions(selectElement, value);
     };
 
     return Promise.resolve({
@@ -64,7 +74,9 @@ describe('User Page', () => {
       getHeader,
       getCurrentPath,
       getCreateUserButton,
-      populateField,
+      populateInput,
+      populateSelect,
+      getUserTypeSelect,
     });
   };
 
@@ -78,6 +90,7 @@ describe('User Page', () => {
     const emailInput = await view.getInput('email');
     const typeSelect = await view.getInput('user type');
     const submitButton = await view.getCreateUserButton();
+    const select = await view.getUserTypeSelect();
 
     expect(header).toBeDefined();
     expect(firstNameInput).toBeDefined();
@@ -86,28 +99,43 @@ describe('User Page', () => {
     expect(emailInput).toBeDefined();
     expect(typeSelect).toBeDefined();
     expect(submitButton).toBeDefined();
+    expect(select).toBeDefined();
+
     expect(submitButton.textContent).toBe('Create User');
+    expect(submitButton.disabled).toBe(true);
   });
 
   it('shoud render form and validate submit button when user fills the form', async () => {
     const view = await getView({ initialRoute: '/users/new' });
 
     const header = await view.getHeader();
-    const firstNameInput = await view.getInput('first name');
-    const lastNameInput = await view.getInput('last name');
-    const phoneInput = await view.getInput('phone number');
-    const emailInput = await view.getInput('email');
-    const typeSelect = await view.getInput('user type');
+    const firstNameInput = (await view.getInput('first name')) as HTMLInputElement;
+    const lastNameInput = (await view.getInput('last name')) as HTMLInputElement;
+    const phoneInput = (await view.getInput('phone number')) as HTMLInputElement;
+    const emailInput = (await view.getInput('email')) as HTMLInputElement;
     const submitButton = await view.getCreateUserButton();
+    const select = (await view.getUserTypeSelect()) as HTMLSelectElement;
 
     expect(header).toBeDefined();
     expect(firstNameInput).toBeDefined();
     expect(lastNameInput).toBeDefined();
     expect(phoneInput).toBeDefined();
     expect(emailInput).toBeDefined();
-    expect(typeSelect).toBeDefined();
     expect(submitButton).toBeDefined();
+    expect(select).toBeDefined();
+
     expect(submitButton.textContent).toBe('Create User');
-    expect(submitButton.disabled).toBe(true);
+
+    await view.populateInput('first name', 'John');
+    await view.populateInput('last name', 'Doe');
+    await view.populateInput('email', 'some@email.fake');
+    await view.populateSelect('type', 'admin');
+
+    expect(firstNameInput.value).toBe('John');
+    expect(lastNameInput.value).toBe('Doe');
+    expect(phoneInput.value).toBe('');
+    expect(emailInput.value).toBe('some@email.fake');
+    expect(select.value).toBe('admin');
+    expect(submitButton.disabled).toBe(false);
   });
 });
