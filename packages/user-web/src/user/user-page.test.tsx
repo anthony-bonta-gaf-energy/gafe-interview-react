432497abf68aad940c46d60131f9303177707462
Luis Alberto P.C. <luisalbertodev@gmail.com>
2025-06-08 14:24:42 -0600

    test: :white_check_mark: make form fields controlled to pass input scenario

--- packages/user-web/src/user/user-page.test.tsx
+++ packages/user-web/src/user/user-page.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { createMemoryHistory, MemoryHistory } from 'history';
 import { Router } from 'react-router';
 import { describe, expect, it } from 'vitest';
@@ -35,9 +36,24 @@ describe('UserPage', () => {
       return Promise.resolve(screen.getByLabelText(label));
     };
 
+    const fillInput = async (label: RegExp, value: string) => {
+      const field = await getInput(label);
+
+      await userEvent.clear(field);
+      await userEvent.type(field, value);
+    };
+
+    const selectOption = async (label: RegExp, value: string) => {
+      const select = await getSelect(label);
+      await userEvent.selectOptions(select, value);
+    };
+
     return Promise.resolve({
       getInput,
       getSelect,
+
+      fillInput,
+      selectOption,
     });
   };
 
@@ -62,7 +78,39 @@ describe('UserPage', () => {
         expect(phoneInput.value).toBe('');
         expect(typeSelect.value).toBe('');
       });
-      it('should allow the user to type in the form fields', () => {});
+      it('should allow the user to type in the form fields', async () => {
+        // Arrange
+        const mockUser = {
+          firstName: 'John',
+          lastName: 'Doe',
+          phoneNumber: '1234567890',
+          email: 'john.doe@example.com',
+          type: 'admin',
+        };
+
+        const history = createMemoryHistory({ initialEntries: ['/users/new'] });
+        const view = await getView({ history });
+
+        // Act
+        await view.fillInput(/First Name/i, mockUser.firstName);
+        await view.fillInput(/Last Name/i, mockUser.lastName);
+        await view.fillInput(/Phone Number/i, mockUser.phoneNumber);
+        await view.fillInput(/Email/i, mockUser.email);
+        await view.selectOption(/Type/i, mockUser.type);
+
+        // Assert
+        const firstNameInput = await view.getInput(/First Name/i);
+        const lastNameInput = await view.getInput(/Last Name/i);
+        const emailInput = await view.getInput(/Email/i);
+        const phoneInput = await view.getInput(/Phone Number/i);
+        const typeSelect = await view.getSelect(/Type/i);
+
+        expect(firstNameInput.value).toBe(mockUser.firstName);
+        expect(lastNameInput.value).toBe(mockUser.lastName);
+        expect(emailInput.value).toBe(mockUser.email);
+        expect(phoneInput.value).toBe(mockUser.phoneNumber);
+        expect(typeSelect.value).toBe(mockUser.type);
+      });
     });
   });
 
--- packages/user-web/src/user/user-page.tsx
+++ packages/user-web/src/user/user-page.tsx
@@ -1,6 +1,13 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import { UserType } from './user.mjs';
 
 export function UserPage() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [type, setType] = useState<UserType | ''>('');
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // TODO: save user
@@ -13,23 +20,52 @@ export function UserPage() {
       <form aria-label="User Form" onSubmit={handleSubmit}>
         <div className="form-field">
           <label htmlFor="firstName">First Name</label>
-          <input id="firstName" name="firstName" type="text" />
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            value={firstName}
+            onChange={e => setFirstName(e.target.value)}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="lastName">Last Name</label>
-          <input id="lastName" name="lastName" type="text" />
+          <input
+            id="lastName"
+            name="lastName"
+            type="text"
+            value={lastName}
+            onChange={e => setLastName(e.target.value)}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="phoneNumber">Phone Number</label>
-          <input id="phoneNumber" name="phoneNumber" type="tel" />
+          <input
+            id="phoneNumber"
+            name="phoneNumber"
+            type="tel"
+            value={phoneNumber}
+            onChange={e => setPhoneNumber(e.target.value)}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
         </div>
         <div className="form-field">
           <label htmlFor="type">Type</label>
-          <select id="type" name="type">
+          <select
+            id="type"
+            name="type"
+            value={type}
+            onChange={e => setType(e.target.value as UserType)}
+          >
             <option value="">Select...</option>
             <option value="admin">Admin</option>
             <option value="basic">Basic</option>
