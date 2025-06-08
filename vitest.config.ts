d07eb5ed07db724386c2d04d019d7bf2bde23204
Luis Alberto P.C. <luisalbertodev@gmail.com>
2025-06-08 15:59:48 -0600

    chore: :wrench: add jest-dom for enhanced DOM assertions

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,16 @@
-import tsConfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
   test: {
-    environment: "jsdom",
+    environment: 'jsdom',
     testTimeout: 30000,
+    setupFiles: ['./packages/user-web/src/testSetup.mts'],
+    globals: true,
     coverage: {
       all: false,
-      provider: "istanbul",
+      provider: 'istanbul',
     },
   },
 });
