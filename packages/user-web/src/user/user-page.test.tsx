59885b31943ac713a87127dee8793922778a7328
Luis Alberto P.C. <luisalbertodev@gmail.com>
2025-06-08 14:05:47 -0600

    test: :white_check_mark: assert empty state of all fields in user form

--- packages/user-web/src/user/user-page.test.tsx
+++ packages/user-web/src/user/user-page.test.tsx
@@ -1,8 +1,69 @@
-import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory, MemoryHistory } from 'history';
+import { Router } from 'react-router';
+import { describe, expect, it } from 'vitest';
+import { UserPage } from './user-page';
+
+/**
+ * Feature: Create a New User
+ *
+ * Scenario: Allow me to fill out the form
+ * Given I am on the create user page
+ * Then I should see an empty user form
+ * And I should be able to enter in the information for <Field> of type <Type>
+ */
+
+interface GetViewArgs {
+  history?: MemoryHistory;
+}
 
 describe('UserPage', () => {
-  describe('Create', () => {
-    it('should create the user when the Save button is clicked', () => {});
+  const getView = (args?: GetViewArgs) => {
+    const history = args?.history ?? createMemoryHistory({ initialEntries: ['/users/new'] });
+
+    const target = render(
+      <Router location={history.location} navigator={history}>
+        <UserPage />
+      </Router>,
+    );
+
+    const getInput = (label: RegExp): Promise<HTMLInputElement> => {
+      return Promise.resolve(screen.getByLabelText(label));
+    };
+
+    const getSelect = (label: RegExp): Promise<HTMLSelectElement> => {
+      return Promise.resolve(screen.getByLabelText(label));
+    };
+
+    return Promise.resolve({
+      getInput,
+      getSelect,
+    });
+  };
+
+  describe('Feature: Create a New User', () => {
+    describe('Scenario: Allow me to fill out the form', () => {
+      it('should see an empty user form', async () => {
+        // Arrange
+        const history = createMemoryHistory({ initialEntries: ['/users/new'] });
+        const view = await getView({ history });
+
+        // Act
+        const firstNameInput = await view.getInput(/First Name/i);
+        const lastNameInput = await view.getInput(/Last Name/i);
+        const emailInput = await view.getInput(/Email/i);
+        const phoneInput = await view.getInput(/Phone Number/i);
+        const typeSelect = await view.getSelect(/Type/i);
+
+        // Assert
+        expect(firstNameInput.value).toBe('');
+        expect(lastNameInput.value).toBe('');
+        expect(emailInput.value).toBe('');
+        expect(phoneInput.value).toBe('');
+        expect(typeSelect.value).toBe('');
+      });
+      it('should allow the user to type in the form fields', () => {});
+    });
   });
 
   describe('Update', () => {
--- packages/user-web/src/user/user-page.tsx
+++ packages/user-web/src/user/user-page.tsx
@@ -1,3 +1,41 @@
+import { FormEvent } from 'react';
+
 export function UserPage() {
-  return <div className="modify-user-page">User Page</div>;
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // TODO: save user
+  };
+
+  return (
+    <div>
+      <h1>User Page</h1>
+
+      <form aria-label="User Form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label htmlFor="firstName">First Name</label>
+          <input id="firstName" name="firstName" type="text" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="lastName">Last Name</label>
+          <input id="lastName" name="lastName" type="text" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input id="phoneNumber" name="phoneNumber" type="tel" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" />
+        </div>
+        <div className="form-field">
+          <label htmlFor="type">Type</label>
+          <select id="type" name="type">
+            <option value="">Select...</option>
+            <option value="admin">Admin</option>
+            <option value="basic">Basic</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
 }
