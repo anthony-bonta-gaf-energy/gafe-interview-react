66d808c041f31c7d1a64d6dd478c352ac4816e5c
Anthony <anthony.bonta@gaf.energy>
2025-06-09 09:58:02 -0500

    build: move the build forward

--- packages/user-web/vite.config.ts
+++ packages/user-web/vite.config.ts
@@ -1,2 +1,14 @@
-import { defineWeb } from '../../.config/vite-web';
-export default defineWeb(__dirname);
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsConfigPaths()],
+  server: {
+    strictPort: true,
+  },
+  resolve: {
+    alias: {
+      lodash: 'lodash-es',
+    },
+  },
+});
